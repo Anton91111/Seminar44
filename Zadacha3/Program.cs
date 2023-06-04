d5e346bf4dcdf808d6c41fc54339f273627d59e9
Anton <anton.esev@mail.ru>
2023-06-04 08:01:22 +0300

    new

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] array1 = new int[] { 1, 2, 5, 7, 19 };
+Console.WriteLine($"array1 -> [{string.Join(", ", array1)}]");
+int[] array2 = new int[] { 6, 1, 33 };
+Console.WriteLine($"array2 -> [{string.Join(", ", array2)}]");
